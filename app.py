b0fe35311cf61e50d82ea9866430132092adb972
octojon <mail@octojon.dev>
2025-10-14 13:43:27 -0400

    added data display example

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+class Material:
+    # define class variables
+    name="Default Name"
+    cost=0.00
+    description="A default description to display on the screen."
+    quantity=0
+    # write the constructor
+    def __init__(self):
+        pass
+
+a = Material()
+a.name = "Red Oak"
+a.cost = 7.25
+
+d = [ Material(), a ] # make a list with an object in it
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/view-materials")
+@app.route("/materials")
 def view_materials():
-    return render_template('view-materials.html')
-
-@app.route("/submit-material", methods=["POST"])
-def submit():
-    first_name = request.form["first_name"]
-    email = request.form["email"]
-    print(f"Got POST request from {first_name} with email {email}")
-    return "form submitted"
-
-@app.route("/add-material")
-def add_material():
-    return render_template("form.html")
+    return render_template('view-materials.html', materials=d)
