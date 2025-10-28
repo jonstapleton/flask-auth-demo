758d896ebc216df1c04c2759d6fe095f95ee5e48
octojon <mail@octojon.dev>
2025-10-28 12:55:40 -0400

    added database

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request, redirect
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sqlalchemy.orm as so
+import sqlalchemy as sa
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'app.db')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class Material:
+class Material(db.Model):
     # define class variables
-    name="Default Name"
-    cost=0.00
-    description="A default description to display on the screen."
-    quantity=0
+    id:so.Mapped[int] = so.mapped_column(primary_key = True)
+    cost:so.Mapped[float] = so.mapped_column(index=True, default=0.00)
+    description:so.Mapped[str] = so.mapped_column(index=True, default="A default description to display on the screen.")
+    quantity:so.Mapped[int] = so.mapped_column(index=True, default=0)
     # write the constructor
     def __init__(self):
         pass
@@ -30,6 +40,8 @@ def view_materials():
     elif request.method == 'POST':
         # do post stuff, like store form field data
         print(f"Form submitted with name {request.form["name"]}")
-        pass
+        o = Material()
+        o.name = request.form["name"]
+        d.append(o) # add o to the list of Materials
     
     return render_template('view-materials.html', materials=d)
