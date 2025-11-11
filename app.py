7361b27b9215f89f31faad02d5a9d730c6c352c6
octojon <mail@octojon.dev>
2025-11-11 13:58:56 -0500

    user auth example

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 import os
+from typing import Optional
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sqlalchemy.orm as so
@@ -9,37 +12,40 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, 'app.db')
+app.secret_key = "a secret"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
-class Material(db.Model):
-    # define class variables
-    id:so.Mapped[int] = so.mapped_column(primary_key = True)
-    cost:so.Mapped[float] = so.mapped_column(index=True, default=0.00)
-    description:so.Mapped[str] = so.mapped_column(index=True, default="A default description")
-    quantity:so.Mapped[int] = so.mapped_column(index=True, default=0)
-    # write the constructor
-    def __init__(self):
-        pass
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key = True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-@app.route('/')
+    def set_password(self, new_password):
+        self.password = generate_password_hash(new_password)
+
+    def check_password(self, p):
+        return check_password_hash(self.password, p)
+
+@login.user_loader
+def loader(id):
+    return db.session.get(User, int(id))
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
-
-@app.route("/materials", methods=["GET", "POST"])
-def view_materials():
-    # create a database query
-    query = sa.select(Material)
-    d = db.session.scalars(query).all()
-    
     if request.method == 'GET':
-        # do GET stuff
-        pass
-    elif request.method == 'POST':
-        # do post stuff, like store form field data
-        print(f"Form submitted with name {request.form["name"]}")
-        obj = Material()
-        db.session.add(obj)
+        return render_template('form.html')
+    elif request.method == "POST":
+        if current_user.is_authenticated:
+            return 'already logged in!'
+        user = db.session.scalar(
+            sa.select(User).where(User.email== request.form["email"])
+        )
+        if user is None or not user.check_password(request.form["password"]):
+            return 'login failed!'
+        login_user(user)
+        return 'login succeeded!'
+    return 'ugh'        
 
-    db.session.commit() # commit changes at the end of the route!
-    return render_template('view-materials.html', materials=d)
